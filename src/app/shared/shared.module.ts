4cb5d27790539c7457d6b874b3ef472af04973ce
oslobird <andrey940@gmail.com>
2019-08-08 19:18:31 +0300

    fonts, assets, styles + routing, modules, header, footer

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Components
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ]
+})
+export class SharedModule { }
